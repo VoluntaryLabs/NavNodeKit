1523cfbd1d4ee4167810c94046688573803ee5a1
Steve Dekorte <steve@dekorte.com>
2014-04-21 16:31:30 -0700

    Adding classes and project

---
+++ NavNodeKit/Classes/NavNode.h
@@ -0,0 +1,80 @@
+//
+//  NavNode.h
+//  Bitmarket
+//
+//  Created by Steve Dekorte on 1/31/14.
+//  Copyright (c) 2014 Bitmarkets.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <FoundationCategoriesKit/FoundationCategoriesKit.h>
+
+@class NavNode;
+
+@protocol NodeViewProtocol <NSObject>
+- (void)setNode:(id)aNode;
+@end
+
+
+@interface NavNode : NSObject
+
+@property (assign, nonatomic) NavNode *nodeParent;
+@property (strong, nonatomic) NSMutableArray *children;
+//@property (strong, nonatomic) NSMutableArray *actions;
+@property (strong, nonatomic) NSView *nodeView;
+@property (assign, nonatomic) BOOL shouldSelectChildOnAdd;
+@property (assign, nonatomic) BOOL shouldSortChildren;
+
+- (NSUInteger)nodeDepth;
+
+// children
+
+- (void)addChild:(id)aChild;
+- (void)removeChild:(id)aChild;
+- (void)sortChildren;
+
+// inlining
+
+@property (assign, nonatomic) BOOL shouldInlineChildren;
+- (NSArray *)inlinedChildren;
+- (BOOL)nodeParentInlines;
+- (BOOL)nodeShouldIndent;
+- (CGFloat)nodeSuggestedRowHeight;
+
+- (NavNode *)childWithTitle:(NSString *)aTitle;
+- (NSArray *)nodeTitlePath:(NSArray *)pathComponents;
+
+- (NSString *)nodeTitle;
+- (NSImage *)nodeIconForState:(NSString *)aState;
+
+- (BOOL)isRead;
+
+//- (CGFloat)nodeSuggestedWidth;
+
+- (void)deepFetch;
+- (void)fetch;
+- (void)refresh;
+
+- (void)postParentChanged;
+- (void)postSelfChanged;
+
+- (id)childWithAddress:(NSString *)address; // hack - move to node subclass
+
+// --- search ---
+
+@property (assign, nonatomic) BOOL isSearching;
+@property (strong, nonatomic) NSMutableArray *searchResults;
+
+- (BOOL)canSearch;
+- (void)search:(NSString *)aString;
+- (BOOL)nodeMatchesSearch:(NSString *)aString;
+
+// actions
+
+- (NSArray *)actions;
+- (NSArray *)modelActions;
+- (NSArray *)uiActions;
+
+- (NSString *)verifyActionMessage:(NSString *)aString;
+
+@end
---
+++ NavNodeKit/NavNodeKit.h
@@ -0,0 +1,11 @@
+//
+//  NavNodeKit.h
+//  NavNodeKit
+//
+//  Created by Steve Dekorte on 4/21/14.
+//  Copyright (c) 2014 voluntary.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "NavNode.h"
